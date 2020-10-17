70069a83836c14fa44a9fbf5a9374c53b81ce433
Nathan Glick <nateleighglick@hotmail.com>
2020-10-17 10:12:01 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -39,6 +39,7 @@ def turn(board)
   user_input = gets.strip.to_i
   index = input_to_index(user_input)
   if valid_move?(board, index)
+    move(board, position, char = "X")
     display_board(board)
 else 
   turn(board)
